34e9cacca1295286ae87fad0093b4fd6a6b46d48
itsisrael <bo66cobra99@gmail.com>
2023-02-06 16:31:20 +0100

    :octocat: improved script v.2 :octocat:

--- installer.sh
+++ installer.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
-
 cd /opt
-
-wait
-
-apt update -y && apt upgrade -y > /dev/null
-
-wait
-
+apt update -y > /dev/null && apt upgrade -y > /dev/null
 cd fogproyect-dev/bin/
-
-wait
-
 chmod u+x ./installfog.sh
-
-wait
-
 ./installfog.sh -X -Y
-
